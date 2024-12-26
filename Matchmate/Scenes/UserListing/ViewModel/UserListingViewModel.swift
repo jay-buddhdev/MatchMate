77c7287f95eeda470f474667e9bebd8f73bc7a4a
Jay Buddhdev <jaybuddhdev240@gmail.com>
2024-12-26 13:30:53 +0530

    Core Data Implementation

--- Matchmate/Scenes/UserListing/ViewModel/UserListingViewModel.swift
+++ Matchmate/Scenes/UserListing/ViewModel/UserListingViewModel.swift
@@ -11,11 +11,15 @@ import Foundation
 class UserListingViewModel: ObservableObject {
     
     // MARK: - Variables
-    @Published var users: [UserList] = []
+    @Published var users: [User] = []
     @Published var isLoading: Bool = false
     var fetchUserError = PassthroughSubject<String, Never>()
     var fetchUserErrorPublisher: AnyPublisher<String, Never> {
         fetchUserError.eraseToAnyPublisher()
+    }
+    var userUpdateError = PassthroughSubject<String, Never>()
+    var userUpdateErrorPublisher: AnyPublisher<String, Never> {
+        userUpdateError.eraseToAnyPublisher()
     }
     
     // MARK: - Functions
@@ -23,16 +27,78 @@ class UserListingViewModel: ObservableObject {
         Task {
             do {
                 let result = try await APIManager.shared.request(endpoint: "/?results=10", responseType: BaseUserListResponse.self)
-                DispatchQueue.main.async { [weak self] in
-                    guard let self else { return }
-                    users = result.results
-                    isLoading = false
-                }
+                saveUserToCoreData(result.results)
             }
             catch {
                 isLoading = false
                 fetchUserError.send(error.localizedDescription)
             }
         }
+    }
+    
+    // MARK: - Functions
+    func saveUserToCoreData(_ user: [UserList]) {
+        let result = CoreDataManager.shared.saveUserToCoreData(from: user)
+        switch result {
+        case .success(_):
+            fetchUsers()
+        case .failure(let error):
+            fetchUserError.send(error.localizedDescription)
+        }
+    }
+    
+    func fetchUsers() {
+        let result = CoreDataManager.shared.fetchAllUsers()
+        switch result {
+        case .success(let users):
+            if users.isEmpty {
+                fetchUsersFromAPI()
+            } else {
+                DispatchQueue.main.async { [weak self] in
+                    guard let self else { return }
+                    self.users = users
+                    isLoading = false
+                }
+            }
+        case .failure(let error):
+            fetchUserError.send(error.localizedDescription)
+        }
+    }
+    
+    func updateUser(userUUID: String, status: String) {
+        let result = CoreDataManager.shared.updateUserStatus(uuid: userUUID, newStatus: status)
+        switch result {
+        case .success:
+            if let index = users.firstIndex(where: { $0.uuid == userUUID }) {
+                users[index].isAccepted = status
+                users = users.map { $0 }
+            }
+        case .failure(let error):
+            userUpdateError.send(error.localizedDescription)
+        }
+    }
+    
+    func updateUserSyncStatus(userUUID: String, status: String) {
+        Task {
+            do {
+                // TODO: replace endPoint and responseMode with updateData endpoint and response Model
+                let result = try await APIManager.shared.request(endpoint: "", responseType: BaseUserListResponse.self)
+                updateUserSyncCoreDataStatus(userUUID: userUUID)
+            }
+            catch {
+                userUpdateError.send(error.localizedDescription)
+            }
+        }
+    }
+    
+    func updateUserSyncCoreDataStatus(userUUID: String) {
+        let result = CoreDataManager.shared.updateUserSyncStatus(uuid: userUUID)
+        switch result {
+        case .success:
+            break
+        case .failure(let error):
+            userUpdateError.send(error.localizedDescription)
+        }
+        
     }
 }
